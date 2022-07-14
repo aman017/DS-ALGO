5ae753ad38730a09ac5c396ea9bbcf490ec78364
aman017 <amankhanna017@gmail.com>
2022-07-15 04:31:42 +0530

    DP day5

--- DSA_450/DSA_450/src/dynamicProgramming/LongSubSeqdiff1.java
+++ DSA_450/DSA_450/src/dynamicProgramming/LongSubSeqdiff1.java
@@ -1,47 +1,23 @@
 package dynamicProgramming;
 
-import java.util.Arrays;
-
 public class LongSubSeqdiff1 {
-
-	public static void main(String[] args) {
-
-		int[] s = { 9, 10, 4, 5, 4, 8, 6 };
-
-		System.out.println(longestSubsequence(7, s));
-	}
-
-	static int[][] dp = new int[1001][1001];
 
 	static int longestSubsequence(int n, int a[]) {
 		// code here
-		for (int[] d : dp) {
-			Arrays.fill(d, -1);
-		}
 
-		int lss = 1;
-		for (int i = 0; i < n; ++i) {
-			lss = Math.max(lss, 1 + solve(a, i+1, a[i], dp));
+		int ans = 0;
+		int dp[] = new int[a.length];
+		for (int i = 0; i < a.length; ++i) {
+			int max = 0;
+			for (int j = 0; j < i; ++j) {
+				if (Math.abs(a[i] - a[j]) == 1) {
+					max = Math.max(max, dp[j]);
+				}
+			}
+			dp[i] = 1 + max;
+			ans = Math.max(ans, dp[i]);
 		}
-		return lss;
+		return ans;
 
 	}
-
-	static int solve(int[] a, int in, int val, int[][] dp) {
-
-		if (in == a.length) {
-			return 0;
-		}
-		if (dp[in][val] != -1) {
-			return dp[in][val];
-		}
-		int sum = 0;
-		if (a[in] == val + 1 || a[in] == val - 1) {
-			sum = sum + 1 + solve(a, in + 1, a[in], dp);
-		} else {
-			sum = sum + solve(a, in + 1, val, dp);
-		}
-		return dp[in][val] = sum;
-	}
-
 }
